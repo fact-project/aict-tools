56d0e97336bd93f6412b394867691614d3fd514e
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-02-06 16:26:25 +0100

    Implement chunked reading for read_hdf

--- klaas/__init__.py
+++ klaas/__init__.py
@@ -1,3 +1,3 @@
-from .io import read_data, pickle_model, write_data
+from .io import read_data, pickle_model, write_data, check_extension
 
-__all__ = ['read_data', 'pickle_model', 'write_data']
+__all__ = ['read_data', 'pickle_model', 'write_data', 'check_extension']
