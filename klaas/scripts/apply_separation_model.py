2f63d29c6b2db98903dba806717cae027ef17ccf
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-02-06 15:28:13 +0100

    Use logging, pep8

--- klaas/scripts/apply_separation_model.py
+++ klaas/scripts/apply_separation_model.py
@@ -1,18 +1,20 @@
 import numpy as np
 import click
-# from IPython import embed
 import joblib
-from klaas import check_extension, read_data, write_data
+from ..io import check_extension, read_data, write_data
 import yaml
+import logging
+
 
 @click.command()
-@click.argument('configuration_path', type=click.Path(exists=True, dir_okay=False, file_okay=True) )
-@click.argument('data_path', type=click.Path(exists=True, dir_okay=False, file_okay=True) )
-@click.argument('model_path', type=click.Path(exists=True, dir_okay=False, file_okay=True) )
-@click.argument('output_path', type=click.Path(exists=False, dir_okay=False, file_okay=True) )
+@click.argument('configuration_path', type=click.Path(exists=True, dir_okay=False))
+@click.argument('data_path', type=click.Path(exists=True, dir_okay=False))
+@click.argument('model_path', type=click.Path(exists=True, dir_okay=False))
+@click.argument('output_path', type=click.Path(exists=False, dir_okay=False))
 def main(configuration_path, data_path, model_path, output_path):
     '''
-    Apply loaded model to data. The cuts applied during model training will also be applied here.
+    Apply loaded model to data.
+    The cuts applied during model training will also be applied here.
 
     CONFIGURATION_PATH: Path to the config yaml file.
 
@@ -22,6 +24,9 @@ def main(configuration_path, data_path, model_path, output_path):
 
     OUTPUT_PATH: Path to the data with added prediction columns.
     '''
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger()
+
     check_extension(output_path)
 
     with open(configuration_path) as f:
@@ -30,28 +35,34 @@ def main(configuration_path, data_path, model_path, output_path):
     training_variables = config['training_variables']
     query = config['query']
 
+    log.info('Loading model')
     model = joblib.load(model_path)
-    #sklearn needs float32 values. after downcasting some -infs appear somehow. here i drop them.
-    print('Loading data')
+
+    log.info('Loading data')
+    # sklearn needs float32 values. Overflows create -infs and infs.
     df_data = read_data(data_path, query=query)
     df_data[training_variables] = df_data[training_variables].astype('float32')
-    df_data = df_data.replace([np.inf, -np.inf], np.nan).dropna(how='any')
 
+    df_data.replace(np.inf, np.finfo('float32').max, inplace=True)
+    df_data.replace(-np.inf, np.finfo('float32').min, inplace=True)
+    df_data.dropna(how='any', inplace=True)
 
-    # embed()
-    print('After dropping nans there are {} events left.'.format(len(df_data)))
-    print('Predicting on data...')
+    log.info('After dropping nans there are {} events left.'.format(len(df_data)))
+
+    log.info('Predicting on data...')
     prediction = model.predict_proba(df_data[training_variables])
-    df_data['signal_prediction'] = prediction[:,1]
+    df_data['signal_prediction'] = prediction[:, 1]
     df_data['signal_theta'] = df_data['Theta']
     df_data['signal_distance'] = df_data['Distance']
+
     if 'Theta' in training_variables:
+        log.info('Theta used in model, redoing classification for off regions')
         thetas = df_data['Theta'].copy()
         distances = df_data['Distance'].copy()
         alphas = df_data['Alpha'].copy()
 
-        print('Predicting off data...')
-        for region in [1,2,3,4,5]:
+        for region in range(1, 6):
+            log.info('Predicting off position {}'.format(region))
             theta_key = 'Theta_Off_{}'.format(region)
             distance_key = 'Distance_Off_{}'.format(region)
             alpha_key = 'Alpha_Off_{}'.format(region)
@@ -59,16 +70,14 @@ def main(configuration_path, data_path, model_path, output_path):
             df_data['Distance'] = df_data[distance_key]
             df_data['Alpha'] = df_data[alpha_key]
             prediction = model.predict_proba(df_data[training_variables])
-            df_data['background_prediction_{}'.format(region)] =  prediction[:,1]
-
+            df_data['background_prediction_{}'.format(region)] = prediction[:, 1]
 
         df_data['Distance'] = distances
         df_data['Theta'] = thetas
         df_data['Alphas'] = alphas
 
-    print('Writing data')
+    log.info('Writing output')
     write_data(df_data, output_path)
-
 
 
 if __name__ == '__main__':
