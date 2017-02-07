c962f0344c6c59eb8b024cee2f37deba77147979
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-02-07 14:50:58 +0100

    Refactor train_energy_regressor to use new sklearn pandas

--- klaas/scripts/train_energy_regressor.py
+++ klaas/scripts/train_energy_regressor.py
@@ -8,6 +8,9 @@ import yaml
 from sklearn import ensemble
 
 from .. import write_data, pickle_model, read_data
+from ..preprocessing import convert_to_float32
+
+import logging
 
 
 @click.command()
@@ -31,7 +34,9 @@ def main(configuration_path, signal_path, predictions_path, model_path):
         If extension is .pmml, then both pmml and pkl file will be saved
     '''
 
-    print("Loading data")
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger()
+
     with open(configuration_path) as f:
         config = yaml.load(f)
 
@@ -42,34 +47,33 @@ def main(configuration_path, signal_path, predictions_path, model_path):
 
     classifier = eval(config['classifier'])
 
+    log.info('Loading data')
     df = read_data(file_path=signal_path, sample=sample, query=query)
+    log.info('Total number of events: {}'.format(len(df)))
 
-    df_train = df[training_variables].astype('float32').replace([np.inf, -np.inf], np.nan).dropna(how='any')
+    df_train = convert_to_float32(df[training_variables])
+    df_train.dropna(how='any', inplace=True)
 
-    df_target = df['MCorsikaEvtHeader.fTotalEnergy']
-    df_target.name = 'true_energy'
-    df_target = df_target[df_train.index]
-    # embed()
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(
-        df_train, df_target, test_size=0.2
-    )
+    log.info('Events after nan-dropping: {} '.format(len(df_train)))
 
-    print('Starting {} fold cross validation... '.format(num_cross_validations) )
+    target = df['MCorsikaEvtHeader.fTotalEnergy']
+    target.name = 'true_energy'
+    target = target.loc[df_train.index]
+
+    log.info('Starting {} fold cross validation... '.format(num_cross_validations))
     scores = []
     cv_predictions = []
 
-    cv = cross_validation.KFold(len(y_train), n_folds=num_cross_validations, shuffle=True)
-    for fold, (train, test) in tqdm(enumerate(cv)):
-        # embed()
-        # select data
-        cv_x_train, cv_x_test = X_train.values[train], X_train.values[test]
-        cv_y_train, cv_y_test = y_train.values[train], y_train.values[test]
-        # fit and predict
-        # embed()
+    kfold = model_selection.KFold(n_splits=num_cross_validations, shuffle=True)
+
+    for fold, (train, test) in tqdm(enumerate(kfold.split(df_train.values))):
+
+        cv_x_train, cv_x_test = df_train.values[train], df_train.values[test]
+        cv_y_train, cv_y_test = df_train.values[train], df_train.values[test]
+
         classifier.fit(cv_x_train, cv_y_train)
         cv_y_prediciton = classifier.predict(cv_x_test)
 
-        # calculate r2 score
         scores.append(metrics.r2_score(cv_y_test, cv_y_prediciton))
 
         cv_predictions.append(pd.DataFrame({
@@ -80,18 +84,19 @@ def main(configuration_path, signal_path, predictions_path, model_path):
 
     predictions_df = pd.concat(cv_predictions, ignore_index=True)
 
-    print('writing predictions from cross validation')
+    log.info('writing predictions from cross validation')
     write_data(predictions_df, predictions_path)
 
     scores = np.array(scores)
-    print("Cross validated R^2 scores: {}".format(scores))
-    print("Mean R^2 score from CV: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()))
+    log.info('Cross validated R^2 scores: {}'.format(scores))
+    log.info('Mean R^2 score from CV: {:0.4f} ± {:0.4f}'.format(
+        scores.mean(), scores.std()
+    ))
 
-    print("Building new model on complete data set...")
-    classifier.fit(X_train, y_train)
-    print("Score on complete data set: {}".format(classifier.score(X_test, y_test)))
+    log.info('Building new model on complete data set...')
+    classifier.fit(df_train.values, target.values)
 
-    print("Pickling model to {} ...".format(model_path))
+    log.info('Pickling model to {} ...'.format(model_path))
     pickle_model(
             classifier=classifier,
             feature_names=list(df_train.columns),
@@ -101,4 +106,4 @@ def main(configuration_path, signal_path, predictions_path, model_path):
 
 
 if __name__ == '__main__':
-    main()
+    main(o)
