6b2b75485827af21e0f0b887840d9f9ede4b2e3a
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-02-28 14:35:11 +0100

    adapt the pmml serialization to new api in sklear2pmml

--- klaas/__init__.py
+++ klaas/__init__.py
@@ -1,12 +1,13 @@
 from os import path
 import pandas as pd
 import json
-from sklearn_pandas import DataFrameMapper
 from sklearn.externals import joblib
-from sklearn2pmml import sklearn2pmml
+from sklearn2pmml import sklearn2pmml, PMMLPipeline
+import numpy as np
+
 
 def write_data(df, file_path, hdf_key='table'):
-    name, extension =  path.splitext(file_path)
+    name, extension = path.splitext(file_path)
     if extension in ['.hdf', '.hdf5', '.h5']:
         df.to_hdf(file_path, key=hdf_key)
     elif extension == '.json':
@@ -14,7 +15,10 @@ def write_data(df, file_path, hdf_key='table'):
     elif extension == '.csv':
         df.to_csv(file_path, delimiter=',', index=False)
     else:
-        raise IOError('cannot write tabular data with format {}. Allowed formats: {}'.format(extension, 'hdf5, json, csv'))
+        raise IOError(
+                'cannot write tabular data with format {}. Allowed formats: {}'
+                .format(extension, 'hdf5, json, csv')
+            )
 
 
 def read_data(file_path, query=None, sample=-1, hdf_key='table'):
@@ -34,7 +38,7 @@ def read_data(file_path, query=None, sample=-1, hdf_key='table'):
 
     else:
         raise Exception('output file forma: {} not supported'.format(extension))
-        
+
     if sample > 0:
         print('Taking {} random samples'.format(sample))
         df = df.sample(sample)
@@ -46,26 +50,30 @@ def read_data(file_path, query=None, sample=-1, hdf_key='table'):
     return df
 
 
-def check_extension(file_path, allowed_extensions= ['.hdf', '.hdf5', '.h5', '.json', '.csv']):
+def check_extension(
+                        file_path,
+                        allowed_extensions=['.hdf', '.hdf5', '.h5', '.json', '.csv'],
+                    ):
     p, extension = path.splitext(file_path)
-    if not extension in allowed_extensions:
+    if extension not in allowed_extensions:
         raise IOError('Allowed formats: {}'.format(allowed_extensions))
 
 
-def pickle_model(classifier, feature_names, model_path, label_text = 'label'):
+def pickle_model(classifier, feature_names, model_path, label_text='label'):
     p, extension = path.splitext(model_path)
     classifier.feature_names = feature_names
     if (extension == '.pmml'):
         print("Pickling model to {} ...".format(model_path))
 
-        mapper = DataFrameMapper([
-                                (feature_names, None),
-                                (label_text, None)
-                        ])
+        joblib.dump(classifier, p + '.pkl', compress=4)
 
+        pipeline = PMMLPipeline([
+            ("classifier", classifier)
+        ])
 
-        joblib.dump(classifier,p + '.pkl', compress = 4)
-        sklearn2pmml(classifier, mapper,  model_path)
+        pipeline.target_field = label_text
+        pipeline.active_fields = np.array(feature_names)
+        sklearn2pmml(pipeline,  model_path)
 
     else:
-        joblib.dump(classifier, model_path, compress = 4)
+        joblib.dump(classifier, model_path, compress=4)
