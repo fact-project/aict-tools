60df59ccc0e5b17bf943d0abc64fedbe60495df1
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-04-10 08:52:07 +0200

    Adapt to new sklearn version

--- klaas/io.py
+++ klaas/io.py
@@ -1,8 +1,8 @@
 from os import path
-from sklearn_pandas import DataFrameMapper
 from sklearn.externals import joblib
-from sklearn2pmml import sklearn2pmml
+from sklearn2pmml import sklearn2pmml, PMMLPipeline
 import logging
+import numpy as np
 
 __all__ = ['pickle_model']
 
@@ -13,14 +13,16 @@ log = logging.getLogger(__name__)
 def pickle_model(classifier, feature_names, model_path, label_text='label'):
     p, extension = path.splitext(model_path)
     classifier.feature_names = feature_names
-    if (extension == '.pmml'):
-        mapper = DataFrameMapper([
-            (feature_names, None),
-            (label_text, None),
-        ])
 
+    if (extension == '.pmml'):
         joblib.dump(classifier, p + '.pkl', compress=4)
-        sklearn2pmml(classifier, mapper,  model_path)
+
+        pipeline = PMMLPipeline([
+            ('classifier', classifier)
+        ])
+        pipeline.target_field = label_text
+        pipeline.active_fields = np.array(feature_names)
+        sklearn2pmml(pipeline, model_path)
 
     else:
         joblib.dump(classifier, model_path, compress=4)
