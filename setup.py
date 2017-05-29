8c0eec45a45cd944ccd54c89cafe4a4340c151b8
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2017-05-29 14:29:54 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='klaas',
-    version='0.2.0',
+    version='0.3.0',
     description='KLAssification And regression Scripts.  yay',
     url='https://github.com/fact-project/klaas',
     author='Kai Brügge',
