b1aa2ae4b934dd258361b12b69a93be536edacfb
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2020-05-26 11:44:40 +0200

    Revert "Removes the energy unit for now"
    This reverts commit 929769a1349a52bc52dc067506e4d71ff67abaf8.

--- aict_tools/configuration.py
+++ aict_tools/configuration.py
@@ -96,6 +96,7 @@ class AICTConfig:
         'energy',
         'separator',
         'has_multiple_telescopes',
+        'energy_unit',
     )
 
     @classmethod
@@ -122,6 +123,7 @@ class AICTConfig:
             self.array_events_key = None
             self.array_event_id_column = None
 
+        self.energy_unit = u.Unit(config.get('energy_unit', 'GeV'))
         self.seed = config.get('seed', 0)
         np.random.seed(self.seed)
 
