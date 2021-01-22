1fc27e2420c33f3e6f10d183578999bc3ddd5329
Arne Poggenpohl <arne.poggenpohl@tu-dortmund.de>
2021-01-22 15:35:39 +0100

    nicer code in dxdy training

--- aict_tools/scripts/train_dxdy_regressor.py
+++ aict_tools/scripts/train_dxdy_regressor.py
@@ -82,7 +82,7 @@ def main(configuration_path, signal_path, predictions_path, dxdy_model_path, key
 
     log.info('Events after nan-dropping: {} '.format(len(df_train)))
 
-    target_dxdy = df.loc[df_train.index, ['true_dx', 'true_dy']]
+    target_dxdy = df.loc[df_train.index, ['true_dx', 'true_dy']].to_numpy()
 
     # load optional columns if available to be able to make performance plots
     # vs true energy / size
@@ -109,8 +109,8 @@ def main(configuration_path, signal_path, predictions_path, dxdy_model_path, key
 
         cv_x_train, cv_x_test = df_train.values[train], df_train.values[test]
 
-        cv_dxdy_train = target_dxdy.values[train]
-        cv_dxdy_test = target_dxdy.values[test]
+        cv_dxdy_train = target_dxdy[train]
+        cv_dxdy_test = target_dxdy[test]
 
         dxdy_regressor.fit(cv_x_train, cv_dxdy_train)
         cv_dxdy_prediction = dxdy_regressor.predict(cv_x_test)
@@ -150,7 +150,7 @@ def main(configuration_path, signal_path, predictions_path, dxdy_model_path, key
     np.random.seed(config.seed)
     dxdy_regressor.random_state = config.seed
 
-    dxdy_regressor.fit(df_train.values, target_dxdy.values)
+    dxdy_regressor.fit(df_train.values, target_dxdy)
 
     log.info('Pickling dxdy model to {} ...'.format(dxdy_model_path))
     save_model(
