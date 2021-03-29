20a1e790b79f5bdf460202845c421a4690136c9e
Lukas Nickel <lukas.nickel@tu-dortmund.de>
2021-03-29 19:32:40 +0200

    Fix table readin in the tests
    - Astropy doesn't seem to like LocalPath objects as it can not infer the
    correct format from that

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,30 +4,32 @@ import pytest
 from aict_tools.configuration import AICTConfig
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def hdf5_file(tmpdir_factory, request):
-    fn = tmpdir_factory.mktemp('aict_test_data').join('test_file.hdf5')
-    shutil.copy('examples/gamma.hdf5', fn)
-    return fn, 'events', AICTConfig.from_yaml('examples/config_energy.yaml')
+    fn = tmpdir_factory.mktemp("aict_test_data").join("test_file.hdf5")
+    shutil.copy("examples/gamma.hdf5", fn)
+    return fn, "events", AICTConfig.from_yaml("examples/config_energy.yaml")
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def cta_file(tmpdir_factory, request):
-    fn = tmpdir_factory.mktemp('aict_test_data').join('cta_file_test.h5')
-    shutil.copy('examples/cta_gammas_diffuse.dl1.h5', fn)
+    fn = tmpdir_factory.mktemp("aict_test_data").join("cta_file_test.h5")
+    shutil.copy("examples/cta_gammas_diffuse.dl1.h5", fn)
     return fn
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def fact_config():
     from aict_tools.configuration import AICTConfig
-    return AICTConfig.from_yaml('examples/config_energy.yaml')
+
+    return AICTConfig.from_yaml("examples/config_energy.yaml")
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def cta_config():
     from aict_tools.configuration import AICTConfig
-    return AICTConfig.from_yaml('examples/cta_full_config.yaml')
+
+    return AICTConfig.from_yaml("examples/cta_full_config.yaml")
 
 
 def test_read_default_columns(hdf5_file):
@@ -64,7 +66,10 @@ def test_read_chunks(hdf5_file):
     from pandas.testing import assert_frame_equal
 
     path, table_name, config = hdf5_file
-    cols = ['width', 'length', ]
+    cols = [
+        "width",
+        "length",
+    ]
 
     chunk_size = 125
     generator = read_telescope_data_chunked(path, config, chunk_size, cols)
@@ -87,35 +92,27 @@ def test_read_chunks_cta_dl1(cta_file, cta_config):
     chunk_size = 500
     # choose some columns from different tables in the file
     columns = [
-        'true_energy',
-        'azimuth',
-        'equivalent_focal_length',
-        'hillas_width',
-        'tel_id',
-        'event_id',
-        'obs_id'
+        "true_energy",
+        "azimuth",
+        "equivalent_focal_length",
+        "hillas_width",
+        "tel_id",
+        "event_id",
+        "obs_id",
     ]
 
+    cta_file = str(cta_file)
     generator = read_telescope_data_chunked(
-        cta_file,
-        cta_config,
-        chunk_size,
-        columns=columns
+        cta_file, cta_config, chunk_size, columns=columns
     )
-    df1 = pd.concat(
-        [df for df, _, _ in generator]
-    )
-    df2 = read_telescope_data(
-        cta_file,
-        cta_config,
-        columns=columns
-    )
+    df1 = pd.concat([df for df, _, _ in generator])
+    df2 = read_telescope_data(cta_file, cta_config, columns=columns)
+
     assert_frame_equal(df1, df2)
 
     # make sure we only loaded the telescopes we wanted
     np.testing.assert_array_equal(
-        df2.tel_id.unique(),
-        [int(x.split('_')[1]) for x in cta_config.telescopes]
+        df2.tel_id.unique(), [int(x.split("_")[1]) for x in cta_config.telescopes]
     )
 
 
@@ -125,11 +122,11 @@ def test_remove_column(hdf5_file):
 
     path, table, _ = hdf5_file
     columns = get_column_names_in_file(path, table)
-    assert 'width' in columns
+    assert "width" in columns
 
-    remove_column_from_file(path, table, 'width')
+    remove_column_from_file(path, table, "width")
     columns = get_column_names_in_file(path, table)
-    assert 'width' not in columns
+    assert "width" not in columns
 
 
 def test_columns_in_file(hdf5_file):
@@ -137,17 +134,17 @@ def test_columns_in_file(hdf5_file):
 
     path, table_name, _ = hdf5_file
     columns = get_column_names_in_file(path, table_name)
-    assert 'width' in columns
-    assert 'length' in columns
+    assert "width" in columns
+    assert "length" in columns
 
 
 def test_read_data(hdf5_file):
     from aict_tools.io import read_data
 
     path, _, _ = hdf5_file
-    df = read_data(path, 'events')
-    assert 'run_id' in df.columns
-    assert 'width' in df.columns
+    df = read_data(path, "events")
+    assert "run_id" in df.columns
+    assert "width" in df.columns
 
 
 def test_append_column(hdf5_file):
@@ -155,7 +152,7 @@ def test_append_column(hdf5_file):
     from aict_tools.io import append_column_to_hdf5
 
     path, table_name, _ = hdf5_file
-    new_column_name = 'foobar'
+    new_column_name = "foobar"
 
     df = read_data(path, table_name)
     assert new_column_name not in df.columns
@@ -173,7 +170,7 @@ def test_append_column_chunked(hdf5_file):
 
     path, table_name, config = hdf5_file
 
-    new_column_name = 'foobar'
+    new_column_name = "foobar"
     chunk_size = 125
 
     df = read_data(path, table_name)
@@ -198,6 +195,7 @@ def test_read_chunks_cta_feature_gen(cta_file, cta_config):
     from aict_tools.io import read_telescope_data_chunked
 
     chunk_size = 100
+    cta_file = str(cta_file)
 
     columns = cta_config.energy.columns_to_read_train
     fg = cta_config.energy.feature_generation
@@ -208,8 +206,8 @@ def test_read_chunks_cta_feature_gen(cta_file, cta_config):
         assert not df.empty
         assert set(df.columns) == set(
             cta_config.energy.features
-            + fg.needed_columns +
-            ['obs_id', 'event_id', 'tel_id']
+            + fg.needed_columns
+            + ["obs_id", "event_id", "tel_id"]
         ) | set([cta_config.energy.target_column])
 
 
@@ -227,4 +225,4 @@ def test_read_telescope_data_feature_gen(hdf5_file, fact_config):
     )
 
     # new column with name 'area' should exist after feature generation
-    assert 'area' in df.columns
+    assert "area" in df.columns
